3a28b51b990f84ad32680fa5215ed983a2048465
Nick Pseudo <tomdop.dakar@gmail.com>
2024-06-03 21:12:16 +0200

    search interface ok

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,66 @@
 function searchFieldClear() {
-  const formField = document.querySelector(".sectionTop__form-field input");
-  const svgclear = document.querySelector(".svg__clear");
+  const formFields = document.querySelectorAll(".form-field input");
 
-  formField.oninput = function () {
-    console.log(formField.value);
-    if (formField.value) {
-      svgclear.style.display = "block";
-    } else {
-      svgclear.style.display = "none";
-    }
-  };
-  svgclear.onclick = function () {
-    formField.value = "";
-    svgclear.style.display = "none";
-  };
+  formFields.forEach(
+    (field) =>
+      (field.oninput = function () {
+        const svgclear = field.parentElement.querySelector(".svg-clear");
+        console.log(field.value);
+        if (field.value) {
+          svgclear.style.display = "block";
+          svgclear.onclick = function () {
+            field.value = "";
+            svgclear.style.display = "none";
+          };
+        } else {
+          svgclear.style.display = "none";
+        }
+      }),
+  );
+}
+
+function setupCustomDropdown() {
+  const customDropdown = document.querySelectorAll(".dropdownBtn");
+
+  customDropdown.forEach((btn) =>
+    btn.addEventListener("click", displayCustomDropdown),
+  );
+
+  function displayCustomDropdown(e) {
+    const currentDropdown = e.target.closest(".dropdownBtn");
+    currentDropdown.nextElementSibling.style.display = "block";
+    currentDropdown.style.borderBottomLeftRadius = "0px";
+    currentDropdown.style.borderBottomRightRadius = "0px";
+    setAngleUp(currentDropdown);
+  }
+  function setAngleUp(target) {
+    const angle = target.querySelector("i");
+    const paragraph = target.querySelector("p");
+    angle.classList.remove("fa-angle-down");
+    angle.classList.add("fa-angle-up");
+    angle.addEventListener("click", hideCustomDropdown);
+    paragraph.addEventListener("click", hideCustomDropdown);
+  }
+  function hideCustomDropdown(e) {
+    e.stopPropagation();
+    const currentDropdown = e.target.closest(".dropdownBtn");
+    const currentList = e.target.parentNode.nextElementSibling;
+    const angle = currentDropdown.querySelector("i");
+    const paragraph = currentDropdown.querySelector("p");
+    console.log(currentDropdown);
+    currentList.style.display = "none";
+    currentDropdown.style.removeProperty("border-bottom-left-radius");
+    currentDropdown.style.removeProperty("border-bottom-right-radius");
+    angle.classList.add("fa-angle-down");
+    angle.classList.remove("fa-angle-up");
+    angle.removeEventListener("click", hideCustomDropdown);
+    paragraph.removeEventListener("click", hideCustomDropdown);
+  }
 }
 
 async function init() {
   searchFieldClear();
+  setupCustomDropdown();
 }
 
 init();
